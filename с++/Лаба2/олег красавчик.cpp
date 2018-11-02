63fd142b811e40f7b3007c45da7d0735a6890c13
Terobit <36206799+olecerhub@users.noreply.github.com>
2018-11-02 09:54:12 +0200

    Update олег красавчик.cpp

--- с++/Лаба2/олег красавчик.cpp
+++ с++/Лаба2/олег красавчик.cpp
@@ -27,35 +27,6 @@ public:
              cash=cash+b;
              cout << "\nОперацiя проведена успішно\n\n";
              }
-    void inf (){#include <stdio.h>
-#include <math.h>
-#include <iostream>
-using namespace std;
-class BankAccount
-{
-private:
-    int cash,rahunok,a,b,c;
-    string valyta;
-    string name1;
-    string name2;
-    string name3;
-public:
-    void mon (int date_cash,string date_valuta ,int date_rahunok){
-    cash = date_cash;
-    rahunok = date_rahunok;
-    valyta = date_valuta;
-    }
-    void inform (string date_name1,string date_name2,string date_name3){
-    name1 = date_name1;
-    name2 = date_name2;
-    name3 = date_name3;
-    }
-    void adde () {
-            cout << "\nВведiть на скiльки ви хочите поповнити баланс\n\n";
-             cin >> b;
-             cash=cash+b;
-             cout << "\nОперацiя проведена успішно\n\n";
-             }
     void inf (){
         cout << "\nНа вашому балансi "<< cash<<" "<< valyta <<"\n\n";
     }
